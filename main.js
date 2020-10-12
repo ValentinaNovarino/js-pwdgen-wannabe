3923c687e876dd5db05cf3ef0ab6714b09997a64
ValentinaNovarino <valenovarino@gmail.com>
2020-10-12 16:34:20 +0200

    fine esercizio

---
+++ main.js
@@ -0,0 +1,10 @@
+var nomeUtente = prompt('Qual è il tuo nome?');
+// console.log(nomeUtente);
+
+var cognomeUtente = prompt('Qual è il tuo cognome?')
+// console.log(cognomeUtente);
+
+var colorePreferito = prompt('Qual è il tuo colore preferito?')
+// console.log(colorePreferito);
+
+document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colorePreferito + '20'
